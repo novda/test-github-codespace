b279e98873cacad1f27fef85ecd6b649f19b3861
zaha <46244346+novda@users.noreply.github.com>
2022-11-14 03:17:48 +0000

    ✨ Test GitHub Codespace

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ function App() {
           GitHub Codespaces <span className="heart">♥️</span> React
         </p>
         <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          <code>Hello world!</code>
+        </p> 
         <p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://t.me/zahadhamov"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Text me
           </a>
         </p>
       </header>
